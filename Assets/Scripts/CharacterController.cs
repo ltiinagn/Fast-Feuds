5dcc615c06fc357ebb9766a99265fc82e672c0ee
ltiinagn <lingtianmsnexplorer@msn.com>
2021-07-08 21:05:40 +0800

    move not instantaneous and invulnerable while moving

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -11,10 +11,12 @@ public class CharacterController : MonoBehaviour
     Dictionary<string, Vector3> keyMap;
 
     Vector3 prevPos;
+    bool invulnerable;
 
     // Start is called before the first frame update
     void Start()
     {
+        invulnerable = false;
         keyMapper = GameObject.Find("KeyMapper");
         keyMap = keyMapper.GetComponent<KeyMapping>().keyMap;
         prevPos = this.transform.position;
@@ -27,32 +29,55 @@ public class CharacterController : MonoBehaviour
         {
             foreach (KeyValuePair<string, Vector3> control in keyMap) {
                 if (Input.GetKeyDown(control.Key)) {
-                    float step = 1 * Time.deltaTime;
-                    gameObject.transform.parent.transform.position = control.Value;
-                    float dist = Vector3.Distance(prevPos, this.transform.position);
-                    Vector3 dir = this.transform.position - prevPos;
-
-                    RaycastHit[] hits;
-                    hits = Physics.RaycastAll(prevPos, dir, dist);
-                    foreach (RaycastHit hit in hits) {
-                        Debug.Log("hit");
-                        hit.transform.gameObject.SendMessage("OnTriggerEnter", hit.collider);
+                    if (control.Value != prevPos && !invulnerable) {
+                        StartCoroutine(moveCharacter(prevPos, control.Value));
                     }
-                    prevPos = this.transform.position;
                 }
             }
         }
     }
 
+    IEnumerator moveCharacter(Vector3 from, Vector3 to) {
+        float startTime = Time.time;
+        float distance = Vector3.Distance(from, to);
+        float speed = 100.0f;
+        invulnerable = true;
+
+        while (true) {
+            float distCovered = (Time.time - startTime) * speed;
+            float fracDist = distCovered / distance;
+            gameObject.transform.parent.gameObject.transform.position = Vector3.Lerp(from, to, fracDist);
+
+            float dist = Vector3.Distance(prevPos, this.transform.position);
+            Vector3 dir = this.transform.position - prevPos;
+
+            RaycastHit[] hits;
+            hits = Physics.RaycastAll(prevPos, dir, dist);
+            foreach (RaycastHit hit in hits) {
+                hit.transform.gameObject.SendMessage("OnTriggerEnter", hit.collider);
+            }
+            prevPos = this.transform.position;
+
+            if (fracDist >= 1) {
+                invulnerable = false;
+                yield break;
+            }
+
+            yield return null;
+        }
+    }
+
     void OnTriggerEnter(Collider col) {
-        Debug.Log("collided");
-        if (col.gameObject.CompareTag("EnemyType1")) {
-            // Debug.Log("damaged by enemy!");
-            // health -= 1;
-            // if (health == 0) {
-            //     Destroy(gameObject);
-            // }
-            onCharacterHit.Invoke();
+        if (!invulnerable) {
+            Debug.Log("collided");
+            if (col.gameObject.CompareTag("EnemyType1")) {
+                // Debug.Log("damaged by enemy!");
+                // health -= 1;
+                // if (health == 0) {
+                //     Destroy(gameObject);
+                // }
+                onCharacterHit.Invoke();
+            }
         }
     }
 
