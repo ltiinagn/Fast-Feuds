fe82ac3a4a0e1642b6ba249c740e77ba0c006653
Wei Ting <hoonweiting@gmail.com>
2021-08-10 22:51:40 +0800

    Add idle and death animation to cake

--- Assets/Scripts/Enemy/ChocolateCakeController.cs
+++ Assets/Scripts/Enemy/ChocolateCakeController.cs
@@ -9,14 +9,14 @@ public class ChocolateCakeController : MonoBehaviour
     public UnityEvent onEnemyDeath;
     private int health;
     private Animator animator;
-    private AudioSource audioSource;
+    // private AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         health = enemyConstants.enemyHealth;
         animator = gameObject.transform.parent.Find("Sprite").GetComponent<Animator>();
-        audioSource = GetComponent<AudioSource>();
+        // audioSource = GetComponent<AudioSource>();
         int direction = Random.Range(0, 2);
         if (direction == 1)
         {
@@ -39,12 +39,11 @@ public class ChocolateCakeController : MonoBehaviour
             if (health == 0)
             {
                 onEnemyDeath.Invoke();
-                // animator.SetTrigger("onDeath");
+                animator.SetTrigger("onDeath");
                 // audioSource.PlayOneShot(audioSource.clip);
                 gameObject.transform.parent.Find("ProjectileChocolateBallSpawner").gameObject.SetActive(false);
                 gameObject.GetComponent<BoxCollider>().enabled = false;
-                Destroy(gameObject.transform.parent.gameObject);
-                // Destroy(gameObject.transform.parent.gameObject, audioSource.clip.length);
+                Destroy(gameObject.transform.parent.gameObject, 1); // audioSource.clip.length);
             }
         }
     }
