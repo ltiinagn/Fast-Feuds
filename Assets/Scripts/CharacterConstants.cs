5e8440b63ece7c08f811cc1c96653a2a63132f67
ltiinagn <lingtianmsnexplorer@msn.com>
2021-08-15 16:06:35 +0800

    reverted character speed

--- Assets/Scripts/CharacterConstants.cs
+++ Assets/Scripts/CharacterConstants.cs
@@ -5,7 +5,7 @@ public class CharacterConstants : ScriptableObject
 {
     // Character
     public int characterHealth = 10;
-    public float characterSpeed = 15.0f;
+    public float characterSpeed = 20.0f;
     public float characterDelay = 0.5f;
     public int characterPowerUpHp = 0;
 }
